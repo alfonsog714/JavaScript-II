7f3e8822f0edc2feba6911d64a7756ff99e284fc
Alfonso <alfonso.garcia2324@gmail.com>
2019-04-02 12:56:07 -0700

    Finished everything except the last challenge on callback.js

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,6 +1,6 @@
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
-const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
+const items = ["Pencil", "Notebook", "yo-yo", "Gum"];
 
 /* 
 
@@ -24,32 +24,61 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 */
 
-
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, length => {
+  console.log(length);
+});
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  cb(arr[arr.length - 1]);
 }
+
+last(items, lastItem => {
+  console.log(lastItem);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+
+sumNums(1, 2, answer => {
+  console.log(answer);
+});
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+
+multiplyNums(3, 3, answer => {
+  console.log(answer);
+});
 
 function contains(item, list, cb) {
+  return cb(
+    list.forEach(current => {
+      if (current === item) {
+        return (cb = true);
+      } else {
+        return (cb = false);
+      }
+    })
+  );
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
-}
+} // function contains
+
+contains("Gum", items, cb => {
+  console.log(cb);
+});
 
 /* STRETCH PROBLEM */
-
-function removeDuplicates(array, cb) {
-  // removeDuplicates removes all duplicate values from the given array.
-  // Pass the duplicate free array to the callback function.
-  // Do not mutate the original array.
-}
+function removeDuplicates(array, cb) {}
+// removeDuplicates removes all duplicate values from the given array.
+// Pass the duplicate free array to the callback function.
+// Do not mutate the original array.
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,9 +1,19 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function outside() {
+  let kevin = "hjo";
+
+  function inside() {
+    let alfonso = "anaheim";
+    console.log(`${kevin} ${alfonso}`);
+  }
+  return inside();
+}
+
+console.log(outside());
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
-
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
